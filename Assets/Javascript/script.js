740a238b114bd4372b1a23a59601bc6d6f147106
TrumanStephenson <stephensontruman96@yahoo.com>
2023-06-15 09:40:25 -0700

    changed terminology for javascript.

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,11 +1,11 @@
 function calculateinterest() {
     // Get user input
     var principal = parseFloat(document.getElementById("principal").value);
-    var timePeriod = parseFloat(document.getElementById("interestRate").value);
-    var timePeriod = parseFloat(document.getElementById("timePeriod").value);
+    var timePeriod = parseFloat(document.getElementById("rate").value);
+    var timePeriod = parseFloat(document.getElementById("time").value);
 
-    // Calculate interest
-    var interest = principal * (interestRate/100) * timePeriod;
+    // Calculate interest 
+    var interest = principal * (interestRate / 100) * timePeriod;
 
     // Calculate total amount
     var totalAmount = principal + interest;
